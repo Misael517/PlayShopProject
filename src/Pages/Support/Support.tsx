f18cf7678245669e3d89f863b67835e5ec9a7a2f
Misael Castillo <MisaelCastillo517@gmail.com>
2023-05-18 16:55:21 -0400

    s

--- src/Pages/Support/Support.tsx
+++ src/Pages/Support/Support.tsx
@@ -1,5 +1,6 @@
 import styles from './Support.module.css';
 import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
 
 function Support() {
     return (
@@ -7,8 +8,12 @@ function Support() {
             <header className={styles.header}>
                 <Navbar />
             </header>
+            <section className={styles.section1}>
 
-
+            </section>
+            <footer className={styles.footer}>
+                <Footer />
+            </footer>
         </>
     )
 }
