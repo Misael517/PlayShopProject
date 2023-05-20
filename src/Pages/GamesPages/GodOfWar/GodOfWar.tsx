4384daa0f003b093a48d45dc38221f88011b7308
Misael Castillo <MisaelCastillo517@gmail.com>
2023-05-19 23:58:55 -0400

    New pages added

---
+++ src/Pages/GamesPages/GodOfWar/GodOfWar.tsx
@@ -0,0 +1,140 @@
+import styles from './GodOfWar.module.css';
+import Navbar from '../../../Components/Navbar/Navbar';
+import Footer from '../../../Components/Footer/Footer';
+import jsonData from '../../../assets/gamesInfo.json';
+import { useState } from 'react';
+
+// Img imports
+import img1 from '/images/gamesImg/GodOfWar/img1.jpg';
+import img2 from '/images/gamesImg/GodOfWar/img2.jpg';
+import img3 from '/images/gamesImg/GodOfWar/img3.jpg';
+import img4 from '/images/gamesImg/GodOfWar/img4.jpg';
+import img5 from '/images/gamesImg/GodOfWar/img5.jpg';
+import img6 from '/images/gamesImg/GodOfWar/img6.jpg';
+import img7 from '/images/gamesImg/GodOfWar/img7.jpg';
+
+// thumbnails imports
+import thumb1 from "/images/gamesImg/GodOfWar/imageThumbnail/thumb1.jpg";
+import thumb2 from "/images/gamesImg/GodOfWar/imageThumbnail/thumb2.jpg";
+import thumb3 from "/images/gamesImg/GodOfWar/imageThumbnail/thumb3.jpg";
+import thumb4 from "/images/gamesImg/GodOfWar/imageThumbnail/thumb4.jpg";
+import thumb5 from "/images/gamesImg/GodOfWar/imageThumbnail/thumb5.jpg";
+import thumb6 from "/images/gamesImg/GodOfWar/imageThumbnail/thumb6.jpg";
+
+interface showCase {
+    id: number;
+    thumbnail: string;
+    image: string;
+}
+
+const imgArr: showCase[] = [
+    {
+        id: 0,
+        thumbnail: thumb1,
+        image: img1
+    },
+    {
+        id: 1,
+        thumbnail: thumb2,
+        image: img2
+    },
+    {
+        id: 2,
+        thumbnail: thumb3,
+        image: img3
+
+    },
+    {
+        id: 3,
+        thumbnail: thumb4,
+        image: img4,
+
+    },
+    {
+        id: 4,
+        thumbnail: thumb5,
+        image: img5
+    },
+    {
+        id: 5,
+        thumbnail: thumb6,
+        image: img6
+    },
+]
+
+
+
+function GodOfWar() {
+    const [currentImg, setCurrentImg] = useState<number>(0)
+
+    return (
+        <>
+            {/* This is the navbar */}
+            <header className={styles.header}>
+                <Navbar />
+            </header>
+
+            {/* This is the main part of the body */}
+            <main className={styles.main}>
+
+
+                <section className={styles.section1}>
+                    {/* This display the images of the current game */}
+                    <div className={styles.imgDisplay} style={{ backgroundImage: `url(${imgArr[currentImg].image}) ` }}>
+                    </div>
+
+                    <div className={styles.imgContainer}>
+                        {imgArr.map((img) => {
+                            return (
+                                <>
+                                    <img src={img.thumbnail} key={img.id} className={`${styles.imgItems} ${currentImg === img.id ? styles.selectedImg : ''}`} onClick={() => setCurrentImg(img.id)} />
+                                </>
+                            )
+                        })}
+                    </div>
+
+
+
+                    {/* This show things like the price and the add to cart button */}
+                    <div className={styles.buyingSection}>
+                        <img src={img7} className={styles.gamePortrait} />
+                        <div className={styles.gamesInfo}>
+
+                            <h3>Starting at:</h3>
+                            <div className={styles.gamesPrice}>
+                                <p><span className={jsonData[13].isOnSale ? styles.discountColor : ''}>{jsonData[13].isOnSale ? `-${jsonData[13].discount}%` : ''}</span></p>
+                                <p><span className={jsonData[13].isOnSale ? styles.strikeThrough : ''}>{jsonData[13].isOnSale ? `${jsonData[13].price}%` : ''}</span></p>
+                                <p style={{ textAlign: 'center' }}>{jsonData[13].isOnSale ? `$${jsonData[13].actualPrice}` : (jsonData[13].coomingSoon ? '...' : `$${jsonData[13].price}`)}</p>
+                            </div>
+                        </div>
+                        <button className={styles.addBtn}><a target="_blank" href={''}></a>Add to cart</button>
+                    </div>
+
+                    {/* This show details about the product */}
+                    <div className={styles.detailsContainer}>
+                        <div>
+                            <h3 className={styles.detailsTitle}>Platforms</h3>
+                            <p className={styles.detailsContent}>{jsonData[13].Platforms}</p>
+                        </div>
+
+                        <div>
+                            <h3 className={styles.detailsTitle}>Publisher</h3>
+                            <p className={styles.detailsContent}>{jsonData[13].Publisher}</p>
+                        </div>
+
+                        <div>
+                            <h3 className={styles.detailsTitle}>Video Game Genre</h3>
+                            <p className={styles.detailsContent}>{jsonData[13].Genre}</p>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+            {/* This is the footer, the end of the page */}
+            <footer className={styles.footer}>
+                <Footer />
+            </footer>
+        </>)
+}
+
+export default GodOfWar
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,18 @@ import './index.css'
 import TheWitcher3 from './Pages/GamesPages/TheWitcher3/TheWitcher.tsx';
 import Gta5 from './Pages/GamesPages/GTA5/Gta5.tsx';
 import EldenRing from './Pages/GamesPages/EldenRing/EldenRing.tsx';
+import Stray from './Pages/GamesPages/Stray/Stray.tsx';
+import HorizonForbidden from './Pages/GamesPages/HorizonForbidden/HorizonForbidden.tsx';
+import PlagueRequiem from './Pages/GamesPages/PlagueRequiem/PlagueRequiem.tsx';
+import TheCalistoProtocol from './Pages/GamesPages/TheCalistoProtocol/TheCalistoProtocol.tsx';
+import NeeForSpeed from './Pages/GamesPages/NeedForSpeed/NeedForSpeed.tsx';
+import GothamKnights from './Pages/GamesPages/GothamKnights/GothamKnights.tsx';
+import HighOnLife from './Pages/GamesPages/HighOnLife/HighOnLife.tsx';
+import SonicFrontiers from './Pages/GamesPages/SonicFrontiers/SonicFrontiers.tsx';
+import Kirby from './Pages/GamesPages/Kirby/Kirby.tsx';
+import DyingLight2 from './Pages/GamesPages/DyingLight2/DyingLight2.tsx';
+import GodOfWar from './Pages/GamesPages/GodOfWar/GodOfWar.tsx';
+import Cyberpunk2077 from './Pages/GamesPages/Cyberpunk2077/Cyberpunk2077.tsx';
 
 const router = createBrowserRouter([
   {
@@ -39,6 +51,54 @@ const router = createBrowserRouter([
   {
     path: "/EldenRing",
     element: <EldenRing />
+  },
+  {
+    path: "/Stray",
+    element: <Stray />
+  },
+  {
+    path: "/HorizonForbiddenWest",
+    element: <HorizonForbidden />
+  },
+  {
+    path: "/APlagueTaleRequiem",
+    element: <PlagueRequiem />
+  },
+  {
+    path: "/TheCalistoProtocol",
+    element: <TheCalistoProtocol />
+  },
+  {
+    path: "/NeedForSpeedUnbound",
+    element: <NeeForSpeed />
+  },
+  {
+    path: "/GothamKnights",
+    element: <GothamKnights />
+  },
+  {
+    path: "/HighOnLife",
+    element: <HighOnLife />
+  },
+  {
+    path: "/SonicFrontiers",
+    element: <SonicFrontiers />
+  },
+  {
+    path: "/KirbyAndTheForgottenLand",
+    element: <Kirby />
+  },
+  {
+    path: "/DyingLight2",
+    element: <DyingLight2 />
+  },
+  {
+    path: "/GodOfWarRagnarok",
+    element: <GodOfWar />
+  },
+  {
+    path: "/Cyberpunk2077",
+    element: <Cyberpunk2077 />
   },
 ])
 
