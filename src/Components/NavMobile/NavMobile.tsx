19f0b5331455c96d1f489362620262eb68c57a9b
Misael Castillo <MisaelCastillo517@gmail.com>
2023-07-04 00:06:49 -0400

    Search bar responsive bug fixed

--- src/Components/NavMobile/NavMobile.tsx
+++ src/Components/NavMobile/NavMobile.tsx
@@ -9,45 +9,31 @@ import logo from '/images/nav/logo.png';
 import cartIcon from '/images/nav/cart.png';
 import profilePic from '/images/nav/Profile.jpg';
 import navMobile from '/navIcon.png';
-import signOutIcon from '/images/nav/signout.png'
 import styles from './NavMobile.module.css'
 
 function NavMobile() {
     useSelector((state: RootState) => state.cart)
-    const [showSingOut, setShowSingOut] = useState<string>('');
-    const [userStatus, setUserStatus] = useState('sing in');
+    useSelector((state: RootState) => state.cart)
+    const mobileNavRef = useRef<HTMLLIElement>(null)
     const [displayMobile, setDisplayMobile] = useState('none');
-    const sessionRef = useRef<HTMLDivElement>(null);
-    const mobileNavRef = useRef<HTMLDivElement>(null)
+    const [showSignIn, setShowSignIn] = useState('');
+    const [userSettings, setUserSettings] = useState('');
     const navigate = useNavigate();
 
 
-    // Close the sign out button when you click outside
-    const handleClickOutside = (event: MouseEvent) => {
-        if (sessionRef.current && !sessionRef.current.contains(event.target as Node)) {
-            setShowSingOut('none');
-        }
-
-        if (mobileNavRef.current && !mobileNavRef.current.contains(event.target as Node) && !(event.target as HTMLElement).classList.contains(styles.navIcon)) {
-            setDisplayMobile('none');
-        }
-    };
-
+    // Check if the 'profileSettings' is stored in local storage
     useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
+        const currentUser = localStorage.getItem('profileSettings');
+        const storedSession = localStorage.getItem('signIn');
 
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
-
-
-    // Check if the userDemo name is stored in local storage
-    useEffect(() => {
-        const storedName = localStorage.getItem('userDemoName');
-        if (storedName) {
-            setUserStatus(storedName);
+        if (currentUser) {
+            setUserSettings(currentUser);
         }
+
+        if (storedSession) {
+            setShowSignIn(storedSession);
+        }
+
     }, []);
 
 
@@ -55,12 +41,14 @@ function NavMobile() {
     const handleSignOut = async () => {
         try {
             await auth.signOut();
-            localStorage.setItem('userDemoName', 'Sing in');
+            localStorage.setItem('profileSettings', 'none');
+            localStorage.setItem('signIn', 'flex');
             window.location.reload();
         } catch (error) {
             console.log(error);
         }
     };
+
 
     // Get the items from the array in the local storage
     const test = localStorage.getItem('cartAmount')
@@ -74,46 +62,50 @@ function NavMobile() {
         <>
             {/* nav mobile */}
             <nav className={styles.navResponsive}>
-                {/* Header logo Mobile*/}
 
-                <div className={styles.navContainerMobile}>
-                    <img src={logo} className={styles.logoMobile} onClick={() => navigate("/")} alt="PlayShop Logo" />
-                    <ul>
+                {/*Logo Mobile*/}
+                <img src={logo} className={styles.logo} onClick={() => navigate("/")} alt="PlayShop Logo" />
+
+
+                <div className={styles.navFrame} style={{ display: displayMobile }}>
+                    <ul className={styles.linksContainer}>
                         <li>
                             <Link to={`/`}>Home</Link>
                         </li>
                         <li>
                             <Link to={`/Discover`}>Discover</Link>
                         </li>
+                        <li className={styles.userFrame} ref={mobileNavRef} style={{ display: displayMobile }}>
+                            <div className={styles.cartContainer}>
+                                <Link to={`/Cart`}>Cart</Link>
+
+                                <div className={styles.cartAmount}>
+                                    <img src={cartIcon} className={styles.cart} onClick={() => navigate('/Cart')} alt="Cart Icon" />
+                                    <p style={{ fontSize: 'calc(2vw + .2rem)' }}>{currentAmount}</p>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
-                </div>
 
-                <div className={styles.navIconContainer}>
-                    <img src={navMobile} alt="navbar" className={styles.navIcon} onClick={() => displayMobile === 'flex' ? setDisplayMobile('none') : setDisplayMobile('flex')} />
-                </div>
-
-                <div className={styles.profileFrameMobile} style={{ display: displayMobile }} ref={mobileNavRef}>
-                    {/* Profile info Mobile*/}
-                    <div className={styles.frameHolderMobile}>
-                        <div className={styles.sessionFrameMobile}>
-                            <img src={profilePic} className={styles.profilePicMobile} alt="Profile Picture" />
-                            <p className={styles.profileNameMobile} onClick={() => auth.currentUser ? setShowSingOut('flex') : navigate('/SingIn')}>{userStatus}</p>
+                    <div className={styles.sessionFrame}>
+                        <div className={styles.profileContainer}>
+                            <div className={styles.profilePic} style={{ display: userSettings, backgroundImage: `url(${profilePic})` }}></div>
+                            <a className={styles.signOutBtn} style={{ display: userSettings }} onClick={() => handleSignOut()}>sign out</a>
+                            <a className={styles.signInBtn} style={{ display: showSignIn }} href="'/SignIn'">Sign In</a>
                         </div>
-
-                        <div className={styles.singOutContainerMobile} style={{ display: showSingOut }} ref={sessionRef}>
-                            <p className={styles.singOutBtnMobile} onClick={() => handleSignOut()}>Sing Out</p>
-                            <img src={signOutIcon} className={styles.signOutIcon} alt="Sign Out Icon" onClick={() => handleSignOut()} />
-                        </div>
-
-                        <div className={styles.cartContainerMobile}>
-                            <p style={{ fontSize: '1.07rem', fontWeight: '100' }} onClick={() => navigate('/Cart')}>Your Cart</p>
-                            <p style={{ fontSize: '0.8rem' }}>{currentAmount}</p>
-                            <img src={cartIcon} className={styles.cartMobile} alt="Cart Icon" />
-                        </div>
-
-                        <SearchBar />
                     </div>
                 </div>
+
+
+                <SearchBar />
+
+
+
+                <img src={navMobile}
+                    alt="navbar"
+                    className={styles.navIcon} onClick={() => displayMobile === 'flex' ? setDisplayMobile('none') : setDisplayMobile('flex')}
+                />
+
             </nav>
 
         </>
